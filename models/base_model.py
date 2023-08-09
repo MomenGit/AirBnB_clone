31575578d132837cdfcedf84530763a844626de6
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-09 14:36:19 +0300

    importing storage instance and integrate it with BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defining the BaseModel module"""
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -18,13 +19,14 @@ class BaseModel:
                     continue
                 if (key == "created_at") or (key == "updated_at"):
                     self.__dict__[key] = datetime.strptime(value,
-                                                   "%Y-%m-%dT%H:%M:%S.%f")
+                                                           "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -43,6 +45,7 @@ class BaseModel:
          updated_at: with the current datetime
          """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing
