7cd9121aae914e7a125b4c73c2eeb405f1ea2e3b
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-09 14:28:17 +0300

    Editing datetime when adding from kwarg

--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,10 @@ class BaseModel:
                 if key == "__class__":
                     continue
                 if (key == "created_at") or (key == "updated_at"):
-                    kwargs[key] = datetime.strptime(value,
+                    self.__dict__[key] = datetime.strptime(value,
                                                    "%Y-%m-%dT%H:%M:%S.%f")
-                self.__dict__[key] = value
+                else:
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
