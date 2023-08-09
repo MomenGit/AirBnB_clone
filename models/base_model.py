6ef216e1bbde940b4e6bddd892537430ab8e0270
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-09 14:03:57 +0300

    Adding __init__ to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
-""""""
+"""Defining the BaseModel module"""
+from datetime import datetime
+import uuid
+
+class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+        """ initaizing BaseModel instance """
+        if kwargs:
+             for key, value in kwarg.items():
+                if key == "__class__":
+                    continue
+                if (key == "created_at") or (key == "updated_at"):
+                    kwarg[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                self.__dict__[key] = value
+        else:
+            self.id = str(uuyid.uuid4())
+            self.crated_at = datetime.now()
+            self.updated_at = datetime.now()
