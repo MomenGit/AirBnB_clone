3ab1375d413a723f66c939a6827bf875dd85a117
momengit <51290928+MomenGit@users.noreply.github.com>
2023-08-10 21:24:35 +0300

    Add dispatch_dict function

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,29 @@
 __all__ = ["base_model", "amenity", "city", "place", "review", "state", "user"]
 from models.engine.file_storage import FileStorage
 
-
 storage = FileStorage()
 storage.reload()
+
+
+def dispatch_dict():
+    """Returns a dictionary mapping
+    classes constructors to their names
+    """
+
+    from .base_model import BaseModel
+    # from models.user import User
+    # from models.city import City
+    # from models.state import State
+    from .amenity import Amenity
+    from .place import Place
+    from .review import Review
+
+    return {
+        "BaseModel": BaseModel,
+        # "User": User,
+        # "City": City,
+        # "State": State,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review,
+    }
