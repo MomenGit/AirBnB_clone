e42836f1d5ff6354b1a1e55378efb70ac7a89c44
momengit <51290928+MomenGit@users.noreply.github.com>
2023-08-10 19:13:06 +0300

    Write models/amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""""""
-from base_model import BaseModel
+"""Defines the Amenity Module"""
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """"""
-    pass
+    """A class represents an Amenity Object"""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
