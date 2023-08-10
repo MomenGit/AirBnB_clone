fbec29a4b88b39c12fb4b349d1a394cf78b857d4
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-10 23:41:01 +0300

    Add class User attributes

--- models/user.py
+++ models/user.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-""""""
-from base_model import BaseModel
+"""user module"""
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """"""
-    pass
+    """A class User that inherits from BaseModel"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
