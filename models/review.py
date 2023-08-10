5b9ac35411a6e11959b00f865a1a055e6f110e87
momengit <51290928+MomenGit@users.noreply.github.com>
2023-08-10 19:13:06 +0300

    Write models/review.py

--- models/review.py
+++ models/review.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-""""""
-from base_model import BaseModel
+"""Defines the Review Module"""
+from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """"""
-    pass
+    """A class represents a Review Object"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
