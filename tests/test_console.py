330eb7a374ed20585ab3b2f71b35ddb3703d8318
momengit <51290928+MomenGit@users.noreply.github.com>
2023-08-13 20:54:47 +0300

    Add update_place test

--- tests/test_console.py
+++ tests/test_console.py
@@ -322,6 +322,19 @@ class TestConsole(unittest.TestCase):
                     output = file.getvalue().strip()
                     self.assertIn('name', output)
 
+    def test_update_place(self):
+        """Test update command"""
+        with patch('sys.stdout', new=StringIO()) as file:
+            self.console.onecmd("create Place")
+            model_id = file.getvalue()
+            with patch('sys.stdout', new=StringIO) as file:
+                self.console.onecmd(
+                    "update Place {} name 'Betty'".format(model_id))
+                with patch('sys.stdout', new=StringIO()) as file:
+                    self.console.onecmd("show Place {}".format(model_id))
+                    output = file.getvalue().strip()
+                    self.assertIn('name', output)
+
     def test_count(self):
         """Test count command"""
         with patch('sys.stdout', new=StringIO()) as file:
