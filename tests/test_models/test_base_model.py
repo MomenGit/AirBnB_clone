4911cca4a89896f4d8948f178ecceb095c4c1f99
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-08-15 07:55:22 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -40,7 +40,7 @@ class Test_BaseModel(unittest.TestCase):
         """Check updated at type"""
         self.assertEqual(type(self.test_model.updated_at), datetime.datetime)
 
-    def test_change_updated_at(self):
+    def test_save(self):
         """Check if updated_at changed when using .save()"""
         self.test_model.save()
         self.assertTrue(self.test_model.created_at <
